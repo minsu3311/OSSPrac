c023f99e52d081e1c55fe3d57fe082693a45f12f
minsu3311 <chssal1792@gmail.com>
2026-04-09 14:42:34 +0900

    modify source code for ossprac2

--- subject5/main.cpp
+++ subject5/main.cpp
@@ -5,6 +5,8 @@ int main() {
     string name;
     string id;
     string id2;
+    string grade;
+    
     cout << "이름을 입력하세요: ";
     cin >> name;
 
@@ -14,10 +16,14 @@ int main() {
     cout << "학과을 입력하세요: ";
     cin >> id2;
 
+    cout << "학년을 입력하세요: ";
+    cin >> grade;
+
     cout << "\n<출력>" << endl;
     cout << "이름: " << name << endl;
     cout << "학번: " << id << endl;
     cout << "학과: " << id2 << endl;
+    cout << "학년: " << grade << endl;
 
     return 0;
 }
